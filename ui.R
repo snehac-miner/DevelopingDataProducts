fab0325fae738422fbd747a83ef6e722a6340350
snehachalla1991 <snehachalla.2k3@gmail.com>
2015-07-26 15:59:36 -0400

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,24 @@
+#ui.R
+#This application calculates the volume and surface area of a cuboid given length ,breadth
+# and height.
+
+shinyUI(pageWithSidebar(
+  headerPanel("To calculate volume and surface area of Cuboid"),
+  sidebarPanel(
+    
+    textInput(inputId='length','Input Length', value='0'),
+    textInput(inputId='breadth','Input Breadth', value='0'),
+    textInput(inputId='height','Input Height', value='0'),
+    dateInput("date","Date:")
+    
+  ),
+  mainPanel(h2('Calculating volume and surface area here'),
+            h4('Volume is'),
+            verbatimTextOutput("oid1"),
+            h4('Surface Area'),
+            verbatimTextOutput("oid2"),
+            h4('The date today is'),
+            verbatimTextOutput("odate")
+#             plotOutput('newHist')
+#             
+  )))
